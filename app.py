23b9d53e236a2e43fd19b723dabbbac93ba7f7c2
hkagankeskin <142995383+hkagankeskin@users.noreply.github.com>
2026-05-14 11:59:38 +0300

    Comment out password check in main loop

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ def check_password():
     return False
 
 # --- 3. UYGULAMA ANA DÖNGÜSÜ ---
-if True # check_password():
+if True 
+    # check_password():
     # Şifre doğruysa uygulama içeriği yüklenir
 
     # Kurumsal CSS
